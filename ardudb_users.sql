fb27e912614e1b3b88fb42485c778b1c8433fe98
Jeremy Andy F. Ampatin <106399501+ampats04@users.noreply.github.com>
2023-05-20 16:42:20 +0800

    Update ardudb_users.sql

--- ardudb_users.sql
+++ ardudb_users.sql
@@ -23,14 +23,12 @@ DROP TABLE IF EXISTS `users`;
 /*!40101 SET @saved_cs_client     = @@character_set_client */;
 /*!50503 SET character_set_client = utf8mb4 */;
 CREATE TABLE `users` (
-  `user_id` int NOT NULL AUTO_INCREMENT,
-  `name` varchar(70) DEFAULT NULL,
-  `birthdate` varchar(255) DEFAULT NULL,
-  `email` varchar(255) DEFAULT NULL,
-  `username` varchar(50) DEFAULT NULL,
-  `password` varchar(50) DEFAULT NULL,
+  `user_id` varchar(50) NOT NULL,
+  `name` varchar(100) NOT NULL,
+  `birthdate` varchar(70) NOT NULL,
+  `username` varchar(50) NOT NULL,
   PRIMARY KEY (`user_id`)
-) ENGINE=InnoDB AUTO_INCREMENT=12 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
 /*!40101 SET character_set_client = @saved_cs_client */;
 
 --
@@ -39,7 +37,7 @@ CREATE TABLE `users` (
 
 LOCK TABLES `users` WRITE;
 /*!40000 ALTER TABLE `users` DISABLE KEYS */;
-INSERT INTO `users` VALUES (1,'Jeremy Andy Ampatin','10-04-01','jeremyandyampatin@gmail.com','ampats11','Jeremy123'),(9,'Michael Oliva Tonilon','October 69, 2001','mtonilon@oten.com','otin123','oten123'),(10,'Michael Oliva Tonilon','October 69, 2001','mtonilon@oten.com','otin123','oten123'),(11,'Michael Oliva Tonilon','October 69, 2001','mtonilon@oten.com','otin123','oten123');
+INSERT INTO `users` VALUES ('5EvYfhKTi6ds7XxeVHNKKFs2tQT2','asfsafasdfsf','2023-04-26 00:00:00.000','asfafsfsafdafd'),('DLFkovZkjIbSEoGdRe1gVafyUa72','Enrico Cedrick A. Reyes','2023-03-16 00:00:00.000','lewejlablab'),('HKcFDWoT2DbDE8w29K7oDI6ET2n2','Jeremy Andy F. Ampatin','2023-04-08 00:00:00.000','ampats11'),('iuSDPfyxwTQiwfrBsRlivOMo2cz1','Jeremy Andy F. Ampatin','October 4, 2001','ampats11'),('jyGFaiQ6BmXTjOZ5gH49oGCgFwk2','J-Anthony F. Ampatin','2010-03-18','lazertwin999'),('khkh','Jeremy Andy F. Ampatin','October 4, 2001','ampats11'),('khksdfsafsfh','Jeremy Andy F. Ampatin','October 4, 2001','ampats11'),('qweqwqeqwe','Jeremy Andy F. Ampatin','October 4, 2001','ampats11'),('qwersosooqwe','Jeremy Andy F. Ampatin','October 4, 2001','ampats11'),('qwert12341qwe','Jeremy Andy F. Ampatin','October 4, 2001','ampats11'),('uMFbzQIqzDNQOxIdtGIhP7Y60dz2','Jeremy Andy F. Ampatin','2001-10-04','ampats11'),('wK4VxfFfjyZe3Vr3OzD9N3OrvYj2','Raymond Benedict','2023-02-24','ryamond'),('xfi1vlmjYYbn4p5hIN6l5LPPwfx1','Jeremy Andy F. Ampatin','2015-01-01','ampats11');
 /*!40000 ALTER TABLE `users` ENABLE KEYS */;
 UNLOCK TABLES;
 /*!40103 SET TIME_ZONE=@OLD_TIME_ZONE */;
@@ -52,4 +50,4 @@ UNLOCK TABLES;
 /*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
 /*!40111 SET SQL_NOTES=@OLD_SQL_NOTES */;
 
--- Dump completed on 2023-04-22 14:10:18
+-- Dump completed on 2023-05-20 16:40:38
